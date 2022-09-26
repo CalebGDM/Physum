4aaffd10033e4c6a8001ba572d81654367173c49
Caleb Garcia <daikuweb@gmail.com>
2022-09-26 17:14:12 -0500

    feat: implemented a Section sign
    Implementation of a basic Sign for sections

---
+++ src/components/SectionSign/SectionSign.tsx
@@ -0,0 +1,50 @@
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import { Title } from "../../constants/Texts";
+import Colors from "../../constants/Colors";
+import { FontAwesome } from "@expo/vector-icons";
+import { getTheme } from "../Themed";
+
+interface SectionSignPops {
+  label: string;
+  screen?: string;
+}
+
+const SectionSign = (props: SectionSignPops) => {
+  const theme = getTheme();
+  return (
+    <View style={[styles.container, {marginBottom: 20}]}>
+      <Text style={[Title.Secondary, { color: Colors[theme].neutral[900] }]}>
+        {props.label}
+      </Text>
+      {props.screen ? (
+        <>
+          <TouchableOpacity style={styles.container}>
+            
+            <FontAwesome
+              style={styles.icon}
+              name="chevron-right"
+              size={18}
+              color={Colors[theme].neutral[500]}
+            />
+          </TouchableOpacity>
+        </>
+      ) : null}
+    </View>
+  );
+};
+
+export default SectionSign;
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+
+  },
+  icon: {
+    marginTop: 5,
+    marginLeft: 10,
+  },
+});
