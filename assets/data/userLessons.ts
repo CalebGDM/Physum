0be25dd0b9b91cc6ddd9c71621971917d95c0f3f
Caleb Garcia <daikuweb@gmail.com>
2022-09-26 19:28:49 -0500

    chore: added models and dummydata
    Added models and data for Lessons and UserLesson

---
+++ assets/data/userLessons.ts
@@ -0,0 +1,30 @@
+export default [
+    {
+        id: '0',
+        lessonName: 'Leyes de newton',
+        color: '#8459FF',
+        imageUri: 'https://cdn.pixabay.com/photo/2020/03/25/12/33/apple-4967157_960_720.png',
+        progress: 70,
+    },
+    {
+        id: '1',
+        lessonName: 'Movimiento Rectilinio Uniforme',
+        color: '#FF598B',
+        imageUri: 'https://cdn.pixabay.com/photo/2020/03/25/12/33/apple-4967157_960_720.png',
+        progress: 90,
+    },
+    {
+        id: '2',
+        lessonName: 'Electricidad y magnetismo',
+        color: '#FFD159',
+        imageUri: 'https://cdn.pixabay.com/photo/2020/03/25/12/33/apple-4967157_960_720.png',
+        progress: 70,
+    },
+    {
+        id: '3',
+        lessonName: 'Trabajo y Energia',
+        color: '#59CDFF',
+        imageUri: 'https://cdn.pixabay.com/photo/2020/03/25/12/33/apple-4967157_960_720.png',
+        progress: 70,
+    },
+]
---
+++ src/types/models.ts
@@ -0,0 +1,14 @@
+type Lesson = {
+    id: string,
+    lessonName: string,
+    color: string,
+    imageUri: string
+}
+
+type UserLesson = {
+    id: string,
+    lessonName: string,
+    color: string,
+    imageUri: string,
+    progress: number,
+}
