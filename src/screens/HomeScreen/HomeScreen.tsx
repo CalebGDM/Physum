6697c6364b1e28c036447be428a6940fd112892d
Caleb Garcia <daikuweb@gmail.com>
2022-09-26 19:32:34 -0500

    feat: Added Lessons and userLessons in homeScreen
    Added TopLessonsList and list of userLessons to homeScreen

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,16 +1,41 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, TouchableOpacity, ScrollView } from 'react-native'
 import React from 'react'
+import { getTheme} from '../../components/Themed'
+import SearchBar from '../../components/SearchBar'
+import LessonPreview from '../../components/LessonPreview'
+import lessons from '../../../assets/data/lessons'
+import { FontAwesome } from '@expo/vector-icons';
 import { Title } from '../../constants/Texts'
 import Colors from '../../constants/Colors'
-import { getTheme} from '../../components/Themed'
+import SectionSign from '../../components/SectionSign'
+import FooterList from '../../components/FooterList'
+import UserLessons from '../../components/UserLessons'
+import userLessons from '../../../assets/data/userLessons'
+
 
 
 const HomeScreen = () => {
   const theme = getTheme()
   return (
-    <View>
-      <Text style={[Title.Item, {color: Colors[theme].primary[600]}]}>HomeScreen</Text>
-      <Text style={[Title.Secondary, {color: Colors[theme].neutral[900]}]}>HomeScreen</Text>
+    <View style={{paddingHorizontal: 15}}>
+
+      {/* TopLessonPewviewList */}
+      <FlatList
+        style={{marginVertical: 20}}
+        data={lessons}
+        renderItem={({item}) => <LessonPreview lesson={item}/>}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        ListFooterComponent={<FooterList/>}
+      />
+
+      {/* Tus Lecciones */}
+      <SectionSign label='Tus lecciones' screen='userLessons'/>
+      <FlatList
+        data={userLessons}
+        renderItem={({item}) => <UserLessons userLesson={item}/>}
+
+      />
     </View>
   )
 }
