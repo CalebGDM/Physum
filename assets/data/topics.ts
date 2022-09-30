3847bb0a01b6a18217f8b62aa158871d7d372c6d
Caleb Garcia <daikuweb@gmail.com>
2022-09-30 18:50:53 -0500

    chore: add topic data

---
+++ assets/data/topics.ts
@@ -0,0 +1,30 @@
+export default[
+    {
+        id: "1",
+        title: '1ra Ley',
+        imageUri: 'https://cdn-icons-png.flaticon.com/512/557/557050.png',
+        level: 1,
+        progress: 1
+    },
+    {
+        id: "2",
+        title: '2ra Ley',
+        imageUri: 'https://cdn-icons-png.flaticon.com/512/557/557050.png',
+        level: 2,
+        progress: 1
+    },
+    {
+        id: "3",
+        title: '3ra Ley',
+        imageUri: 'https://cdn-icons-png.flaticon.com/512/557/557050.png',
+        level: 2,
+        progress: 0.2
+    },
+    {
+        id: "4",
+        title: 'Quiz',
+        imageUri: 'https://cdn-icons-png.flaticon.com/512/557/557050.png',
+        level: 3,
+        progress: 0
+    },
+]
