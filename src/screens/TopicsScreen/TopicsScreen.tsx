9d8065fae745d3812951552a31ff599a44dc5859
Caleb Garcia <daikuweb@gmail.com>
2022-09-30 18:52:16 -0500

    feat: add topics screen

---
+++ src/screens/TopicsScreen/TopicsScreen.tsx
@@ -0,0 +1,37 @@
+import { View, Text, FlatList } from 'react-native'
+import React from 'react'
+import TopicNode from '../../components/TopicNode'
+import TopicNodesRow from '../../components/TopicNodesRow'
+import topics from '../../../assets/data/topics'
+import { getCurrentActiveLevel, groupByLevels } from '../../utils/Topics'
+import { getTheme } from '../../components/Themed'
+import { styles } from '../../constants/GeneralStyles'
+import Colors from '../../constants/Colors'
+
+
+const levels = groupByLevels(topics)
+const currentLevel = getCurrentActiveLevel(levels)
+console.log(currentLevel)
+
+const TopicsScreen = () => {
+  const theme = getTheme()
+  return (
+    <View style={[styles.screen, {backgroundColor: Colors[theme].background}]}>
+      <FlatList
+        data={levels}
+        renderItem={({item}) =>(
+          <TopicNodesRow>
+            {
+              item.map((topic) => (
+                <TopicNode topic={topic} key={topic.id} isDisabled={currentLevel < topic.level}/>
+              ))
+            }
+          </TopicNodesRow>
+        )}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
+  )
+}
+
+export default TopicsScreen
