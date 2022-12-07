57dd7b5b9bf305968a152b2137c0a6ff89d526bf
Caleb Garcia <daikuweb@gmail.com>
2022-12-06 18:28:52 -0600

    chore: add amplify

--- App.tsx
+++ App.tsx
@@ -1,26 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { ThemeProvider } from '@shopify/restyle';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ThemeProvider } from "@shopify/restyle";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './src/navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./src/navigation";
+import { Amplify } from "aws-amplify";
+import awsmobile from "./src/aws-exports";
+
+Amplify.configure(awsmobile);
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-  
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
-      
       <SafeAreaProvider>
         <Navigation colorScheme={null} />
-        <StatusBar style='ligth'/>
+        <StatusBar style="ligth" />
       </SafeAreaProvider>
-      
     );
   }
 }
