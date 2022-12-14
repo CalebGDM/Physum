2a8dd71f8ed558395106beca233244ea91141809
Caleb Garcia <daikuweb@gmail.com>
2022-12-14 15:40:20 -0600

    feat: Custom input whith validation

--- src/components/FormInput/FormInput.tsx
+++ src/components/FormInput/FormInput.tsx
@@ -1,31 +1,65 @@
 import { StyleSheet, Text, TextInput, View } from "react-native";
 import React from "react";
 import Colors from "../../constants/Colors";
-import { Forms, Title } from "../../constants/Texts";
+import { Forms, NormalText, Title } from "../../constants/Texts";
 import { FontAwesome } from "@expo/vector-icons";
+import { onChange } from "react-native-reanimated";
+import { Controller } from "react-hook-form";
+import { valuesEqual } from "@aws-amplify/datastore/lib-esm/util";
 
 interface FormInputProps {
-    placeholder: string,
-    icon: string,
-    isPassword?: boolean,
+  placeholder: string;
+  icon: string;
+  isPassword?: boolean;
+  name: string;
+  control: any;
+  rules?: {};
 }
 
-
-const FormInput = ({placeholder, icon, isPassword}: FormInputProps) => {
+const FormInput = ({
+  placeholder,
+  icon,
+  isPassword,
+  name,
+  control,
+  rules = {},
+}: FormInputProps) => {
   return (
-    <View style={styles.input}>
-      <FontAwesome 
-      style={{marginRight: 20}}
-      name={icon} 
-      size={24} 
-      color={Colors.light.neutral[400]} />
-      <TextInput
-        style={[Forms.Input, { color: Colors.light.neutral[400],  width: '85%' }]}
-        placeholderTextColor={Colors.light.neutral[400]}
-        placeholder={placeholder}
-        secureTextEntry={isPassword}
-      />
-    </View>
+    <Controller
+      control={control}
+      name={name}
+      rules={rules}
+      render={({
+        field: { value, onBlur, onChange },
+        fieldState: { error },
+      }) => (
+        <>
+          <View style={[styles.input, error && {borderBottomColor: Colors.light.error}]}>
+            <FontAwesome
+              style={{ marginRight: 20 }}
+              name={icon}
+              size={24}
+              color={error ? Colors.light.error : Colors.light.neutral[400]}
+            />
+            <TextInput
+              style={[
+                Forms.Input,
+                { color: Colors.light.neutral[400], width: "85%" },
+              ]}
+              placeholderTextColor={Colors.light.neutral[400]}
+              placeholder={placeholder}
+              secureTextEntry={isPassword}
+              value={value}
+              onChangeText={onChange}
+              onBlur={onBlur}
+            />
+          </View>
+          {
+            error && <Text style={[NormalText.Bold, styles.error]}>{error.message || 'Error'}</Text>
+          }
+        </>
+      )}
+    />
   );
 };
 
@@ -38,7 +72,15 @@ const styles = StyleSheet.create({
     borderBottomWidth: 3,
     borderBottomColor: Colors.light.neutral[400],
     flexDirection: "row",
-    alignItems: 'center',
-    marginBottom: 20
+    alignItems: "center",
+    marginBottom: 20,
   },
+  error:{
+    color: Colors.light.error, 
+    textAlign: 'left',
+    width: '75%',
+    marginBottom: 10,
+    marginTop: -10
+  }
+
 });
